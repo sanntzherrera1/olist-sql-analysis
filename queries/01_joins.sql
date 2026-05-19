1dd641222150fc32d35d57044b7d6c97b244e748
Luis Herrera <luis.herreraipc@gmail.com>
2026-05-18 23:31:56 -0300

    initial querie exc

--- queries/01_joins.sql
+++ queries/01_joins.sql
@@ -1,1 +1,11 @@
+/*
+1 — ¿Cuáles son las 10 categorías de productos que más revenue generan en órdenes entregadas?
+*/
 
+SELECT  SUM(order_items.price) as Total_Revenue, products.product_category_name
+FROM `olist.products` products
+INNER JOIN `olist.order_items` order_items ON products.product_id = order_items.product_id
+INNER JOIN `olist.orders` orders ON  order_items.order_id = orders.order_id
+WHERE orders.order_status = 'delivered'
+GROUP BY products.product_category_name
+ORDER BY Total_Revenue DESC
